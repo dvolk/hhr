a5dc295e58125a9169cd691a6643a15249c67cc7
admin <denis.volk@gmail.com>
2014-02-12 12:27:15 +0100

    add default

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,11 @@ module Main where
 import System.Environment
 import System.Console.Terminfo
 
+getArgsDefault :: IO [String]
+getArgsDefault = do
+  a <- getArgs
+  return (if null a then ["="] else a)
+
 main :: IO ()
 main = do
   term <- setupTerm "vt100"
@@ -10,5 +15,5 @@ main = do
 
   case cols of
     Nothing -> error "unable to get terminal width"
-    Just n  -> mapM_ (putStrLn . take n . cycle) =<< getArgs
+    Just n  -> mapM_ (putStrLn . take n . cycle) =<< getArgsDefault
 
